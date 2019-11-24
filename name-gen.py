d997e09897f117a5f3bce7a1be2707b3889281fd
Johan Grahnen <johan.grahnen@gmail.com>
2019-11-24 10:45:02 -0800

    Basic bones of the word generator.

--- name-gen.py
+++ name-gen.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
 
+class MarkovWordGenerator:
+    def __init__(self, state_size=1):
+        self.state_size = state_size
+
+    def fit(self, word_list):
+        return self
+
+    def predict(self, seed_string=None, max_len=None):
+        return "Johan"
+
+
 def load_names(file_path, gender='M'):
     # We're expecting name counts on the format provided by
     # the Social Security Administration, e.g. from
@@ -13,4 +24,7 @@ def load_names(file_path, gender='M'):
 
 if __name__ == "__main__":
     baby_names = load_names('yob2018.txt')
-    print(baby_names[:5])
+    print('Top 5 male names 2018: {}'.format(", ".join(baby_names[:5])))
+
+    name_generator = MarkovWordGenerator(state_size=3).fit(baby_names)
+    print('Random name: {}'.format(name_generator.predict()))
